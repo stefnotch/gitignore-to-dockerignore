afae24ddbc692bf71b5f294a20a4abd98dee8a92
Stefnotch <brandmairstefan@gmail.com>
2019-05-06 13:37:56 +0200

    Very simplistic website

---
+++ src/components/GitToDocker.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="flex-row-container">
+    <textarea v-model="gitignore"></textarea>
+    <pre><code>{{ gitToDocker(gitignore) }}</code></pre>
+  </div>
+</template>
+
+<script>
+import gitignoreToDockerignore from "gitignore-to-dockerignore";
+
+export default {
+  name: "git-to-docker",
+  data() {
+    return {
+      gitignore: ""
+    };
+  },
+  methods: {
+    gitToDocker(text) {
+      return gitignoreToDockerignore(text);
+    }
+  }
+};
+</script>
+<style scoped>
+.flex-row-container {
+  display: flex;
+  flex-direction: row;
+  height: 100%;
+}
+
+.flex-row-container > * {
+  flex: 1;
+  background-color: #eeeeee;
+  margin: 24px;
+  padding: 0px;
+  height: calc(100vh - 48px);
+  overflow-y: auto;
+}
+</style>
+
